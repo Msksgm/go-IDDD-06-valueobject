79b7c4fd28af7fadcdd43a3a475290f0773e8115
Msksgm <msksgm3124@gmail.com>
2022-02-19 15:29:42 +0900

    add: nameが101文字以上だったときのテストを追加

--- iddd_identityaccess/domain/model/identity/tenant_test.go
+++ iddd_identityaccess/domain/model/identity/tenant_test.go
@@ -2,11 +2,19 @@ package identity
 
 import (
 	"fmt"
+	"math/rand"
 	"reflect"
 	"testing"
+	"time"
 
 	"github.com/google/uuid"
 )
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+var rs1Letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
 
 func TestNewTenant(t *testing.T) {
 	t.Run("success", func(t *testing.T) {
@@ -57,4 +65,34 @@ func TestNewTenant(t *testing.T) {
 			t.Errorf("tenant should be nil, but %v", tenant)
 		}
 	})
+	t.Run("fail over 100 characters name", func(t *testing.T) {
+		u, err := uuid.NewRandom()
+		if err != nil {
+			t.Fatal(err)
+		}
+		uu := u.String()
+
+		tenantId, err := NewTenantId(uu)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		name := RandString(101)
+		tenant, err := NewTenant(*tenantId, name)
+		want := fmt.Sprintf("tenant.NewTenant(%v, %s): The tenant description must be 100 characters or less.", *tenantId, name)
+		if got := err.Error(); want != got {
+			t.Errorf("got %s, want %s", got, want)
+		}
+		if tenant != nil {
+			t.Errorf("tenant should be nil, but %v", tenant)
+		}
+	})
+}
+
+func RandString(n int) string {
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = rs1Letters[rand.Intn(len(rs1Letters))]
+	}
+	return string(b)
 }
