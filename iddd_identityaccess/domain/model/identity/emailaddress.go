3faa842decc902e01f95465dd6ce3120251e3b6f
Msksgm <msksgm3124@gmail.com>
2022-02-23 09:11:13 +0900

    add: The email address is required.

--- iddd_identityaccess/domain/model/identity/emailaddress.go
+++ iddd_identityaccess/domain/model/identity/emailaddress.go
@@ -1,11 +1,21 @@
 package identity
 
+import (
+	"fmt"
+
+	"github.com/Msksgm/go-IDDD-05-entity/iddd_common/ierrors"
+)
+
 type EmailAddress struct {
 	address string
 }
 
 func NewEmailAddress(address string) (_ *EmailAddress, err error) {
+	defer ierrors.Wrap(&err, "emailaddress.NewEmailAddress(%s)", address)
 	emailAddress := new(EmailAddress)
+	if address == "" {
+		return nil, fmt.Errorf("The email address is required.")
+	}
 	emailAddress.address = address
 	return emailAddress, nil
 }
--- iddd_identityaccess/domain/model/identity/emailaddress_test.go
+++ iddd_identityaccess/domain/model/identity/emailaddress_test.go
@@ -1,19 +1,30 @@
 package identity
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
 )
 
 func TestNewEmailAddress(t *testing.T) {
-	address := "email@sample.com"
-	got, err := NewEmailAddress(address)
-	if err != nil {
-		t.Error(err)
-	}
-	want := &EmailAddress{address: address}
-	if diff := cmp.Diff(got, want, cmp.AllowUnexported(EmailAddress{})); diff != "" {
-		t.Errorf("mismatch (-want, +got):\n%s", diff)
-	}
+	t.Run("success", func(t *testing.T) {
+		address := "email@sample.com"
+		got, err := NewEmailAddress(address)
+		if err != nil {
+			t.Error(err)
+		}
+		want := &EmailAddress{address: address}
+		if diff := cmp.Diff(got, want, cmp.AllowUnexported(EmailAddress{})); diff != "" {
+			t.Errorf("mismatch (-want, +got):\n%s", diff)
+		}
+	})
+	t.Run("fail the email address is required.", func(t *testing.T) {
+		address := ""
+		_, err := NewEmailAddress(address)
+		want := fmt.Sprintf("emailaddress.NewEmailAddress(%s): The email address is required.", address)
+		if got := err.Error(); want != got {
+			t.Errorf("got %s, want %s", got, want)
+		}
+	})
 }
