f3b5ddd44067e3aca325df3871ab56c33fefd95b
Msksgm <msksgm3124@gmail.com>
2022-02-23 11:39:03 +0900

    Add: the validation of Telephone number is required.

--- iddd_identityaccess/domain/model/identity/telephone.go
+++ iddd_identityaccess/domain/model/identity/telephone.go
@@ -1,11 +1,21 @@
 package identity
 
+import (
+	"fmt"
+
+	"github.com/Msksgm/go-IDDD-05-entity/iddd_common/ierrors"
+)
+
 type Telephone struct {
 	number string
 }
 
 func NewTelephone(aNumber string) (_ *Telephone, err error) {
+	defer ierrors.Wrap(&err, "telephone.NewTelephone(%s)", aNumber)
 	telephone := new(Telephone)
+	if aNumber == "" {
+		return nil, fmt.Errorf("Telephone number is required.")
+	}
 	telephone.number = aNumber
 	return telephone, nil
 }
--- iddd_identityaccess/domain/model/identity/telephone_test.go
+++ iddd_identityaccess/domain/model/identity/telephone_test.go
@@ -1,20 +1,31 @@
 package identity
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
 )
 
 func TestNewTelephone(t *testing.T) {
-	number := "09012345678"
-	got, err := NewTelephone(number)
-	if err != nil {
-		t.Fatal(err)
-	}
-	want := &Telephone{number: number}
+	t.Run("success", func(t *testing.T) {
+		number := "09012345678"
+		got, err := NewTelephone(number)
+		if err != nil {
+			t.Fatal(err)
+		}
+		want := &Telephone{number: number}
 
-	if diff := cmp.Diff(want, got, cmp.AllowUnexported(Telephone{})); diff != "" {
-		t.Errorf("mismatch (-want, +got):\n%s", diff)
-	}
+		if diff := cmp.Diff(want, got, cmp.AllowUnexported(Telephone{})); diff != "" {
+			t.Errorf("mismatch (-want, +got):\n%s", diff)
+		}
+	})
+	t.Run("fail Telephone number is required.", func(t *testing.T) {
+		number := ""
+		_, err := NewTelephone(number)
+		want := fmt.Sprintf("telephone.NewTelephone(%s): Telephone number is required.", number)
+		if got := err.Error(); got != want {
+			t.Errorf("got %s, want %s", got, want)
+		}
+	})
 }
