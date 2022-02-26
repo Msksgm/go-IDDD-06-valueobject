ef9ce592e0a29a3db8a14a941346ac8937d9237c
Msksgm <msksgm3124@gmail.com>
2022-02-26 14:21:16 +0900

    add: the validation of startdate is not after than endDate

--- iddd_identityaccess/domain/model/identity/enablement/enablement.go
+++ iddd_identityaccess/domain/model/identity/enablement/enablement.go
@@ -1,6 +1,10 @@
 package enablement
 
-import "time"
+import (
+	"time"
+
+	"github.com/Msksgm/go-IDDD-05-entity/iddd_common/ierrors"
+)
 
 type Enablement struct {
 	enabled   bool
@@ -10,6 +14,10 @@ type Enablement struct {
 
 func NewEnablement(aEnabled bool, aStartDate time.Time, aEndDate time.Time) (*Enablement, error) {
 	enablement := new(Enablement)
+
+	if err := ierrors.NewArgumentFalseError(aStartDate.After(aEndDate), "Enablement start and/or end date is invalid.").GetError(); err != nil {
+		return nil, err
+	}
 
 	enablement.enabled = aEnabled
 	enablement.startDate = aStartDate
--- iddd_identityaccess/domain/model/identity/enablement/enablement_test.go
+++ iddd_identityaccess/domain/model/identity/enablement/enablement_test.go
@@ -1,10 +1,12 @@
 package enablement
 
 import (
-	"fmt"
+	"errors"
+	"reflect"
 	"testing"
 	"time"
 
+	"github.com/Msksgm/go-IDDD-05-entity/iddd_common/ierrors"
 	"github.com/google/go-cmp/cmp"
 )
 
@@ -12,30 +14,47 @@ const (
 	timeFormat = "2006-01-02 15:04:05"
 )
 
-var jst = time.FixedZone("Asia/Tokyo", 9*60*60)
+var (
+	jst                = time.FixedZone("Asia/Tokyo", 9*60*60)
+	argumentFalseError *ierrors.ArgumentFalseError
+)
 
 func TestNewEnablement(t *testing.T) {
-	enabled := true
-	startDate, err := time.ParseInLocation(timeFormat, "2020-01-01 00:00:00", jst)
-	if err != nil {
-		t.Fatal(err)
-	}
-	endDate, err := time.ParseInLocation(timeFormat, "2030-01-01 00:00:00", jst)
-	if err != nil {
-		t.Fatal(err)
-	}
-	fmt.Println("hoge")
-	fmt.Println(startDate)
-	fmt.Println(endDate)
+	t.Run("sucess", func(t *testing.T) {
+		enabled := true
+		startDate, err := time.ParseInLocation(timeFormat, "2020-01-01 00:00:00", jst)
+		if err != nil {
+			t.Fatal(err)
+		}
+		endDate, err := time.ParseInLocation(timeFormat, "2030-01-01 00:00:00", jst)
+		if err != nil {
+			t.Fatal(err)
+		}
 
-	got, err := NewEnablement(enabled, startDate, endDate)
-	if err != nil {
-		t.Fatal(err)
-	}
+		got, err := NewEnablement(enabled, startDate, endDate)
+		if err != nil {
+			t.Fatal(err)
+		}
 
-	want := &Enablement{enabled: enabled, startDate: startDate, endDate: endDate}
-	allowUnexported := cmp.AllowUnexported(Enablement{})
-	if diff := cmp.Diff(want, got, allowUnexported); diff != "" {
-		t.Errorf("mismatch (-want, +got):\n%s", diff)
-	}
+		want := &Enablement{enabled: enabled, startDate: startDate, endDate: endDate}
+		allowUnexported := cmp.AllowUnexported(Enablement{})
+		if diff := cmp.Diff(want, got, allowUnexported); diff != "" {
+			t.Errorf("mismatch (-want, +got):\n%s", diff)
+		}
+	})
+	t.Run("fail", func(t *testing.T) {
+		enabled := true
+		startDate, err := time.ParseInLocation(timeFormat, "2020-01-01 00:00:00", jst)
+		if err != nil {
+			t.Fatal(err)
+		}
+		endDate, err := time.ParseInLocation(timeFormat, "2010-01-01 00:00:00", jst)
+		if err != nil {
+			t.Fatal(err)
+		}
+		_, err = NewEnablement(enabled, startDate, endDate)
+		if !errors.As(err, &argumentFalseError) {
+			t.Errorf("err type:%v, expect type: %v", reflect.TypeOf(err), reflect.TypeOf(&argumentFalseError))
+		}
+	})
 }
